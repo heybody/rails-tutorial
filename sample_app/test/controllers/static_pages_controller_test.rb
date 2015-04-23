ae1a85636efa4cbd523bc8930dc6d517fd9299e7
heybody <ifiamanass@gmail.com>
2015-04-23 14:42:34 +0000

    Eliminate repetition (solves exercise 3.1)

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -24,5 +24,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title","关于|#{@base_title}"
   end
+  
+   test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title","联系|#{@base_title}"
+  end
 
 end
