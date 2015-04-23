5fa4d32fa99df3b0db09a4681619252c01883129
heybody <ifiamanass@gmail.com>
2015-04-23 14:33:35 +0000

    finish chapter3

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -4,17 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","主页"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","帮助"
   end
   
   
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title","关于"
   end
 
 end
