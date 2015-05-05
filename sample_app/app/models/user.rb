188b84d5dc2793398e8f3a857db058af1f3704dc
heybody <ifiamanass@gmail.com>
2015-05-05 12:59:32 +0000

    add user password_digest attr

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum:50}
-  validates :email, presence: true, length: {maximum:256}
+  validates :email, presence: true, 
+                    length: {maximum:256}, 
+                    format: {with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+                    uniqueness: { case_sensitive: false }
 end
