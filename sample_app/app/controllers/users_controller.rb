38165d10ead57a0b0f8947a8250447237b6b85cf
heybody <ifiamanass@gmail.com>
2015-05-09 11:39:05 +0000

    finish user#show scss

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   def show
     @user = User.find(params[:id])
+  end
+  def create
+    @user = User.new(params[:user])    # 不是最终的实现方式
+    if @user.save
+      # 处理注册成功的情况
+    else
+      render 'new'
+    end
   end
 end
